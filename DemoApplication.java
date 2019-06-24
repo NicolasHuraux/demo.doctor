64ab32f7de413d26d40cb8f78432908a5c3a6043
NicolasHuraux <n.huraux@hotmail.com>
2019-06-24 11:45:51 +0200

    ajout du fichier DemoApplication

---
+++ DemoApplication.java
@@ -0,0 +1,57 @@
+package com.wildcodeschool.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+	return  "<ul>"+
+			"<li><a href='/doctor/1'>Doctor 1</a></li>"+
+			"<li><a href='/doctor/2'>Doctor 2</a></li>"+
+			"<li><a href='/doctor/3'>Doctor 3</a></li>"+
+			"<li><a href='/doctor/4'>Doctor 4</a></li>"+
+			"</ul>"
+				;	
+	
+		}
+	
+	@RequestMapping("/doctor/1")
+	@ResponseBody
+	public String doctor1() {
+		return "William Hartnell";	
+	
+		}
+	
+	@RequestMapping("/doctor/2")
+	@ResponseBody
+	public String doctor2() {
+		return "Christopher Eccleston";	
+	
+		}
+	
+	@RequestMapping("/doctor/3")
+	@ResponseBody
+	public String doctor3() {
+		return "Jodie Whittaker";	
+	
+		}
+	
+	@RequestMapping("/doctor/4")
+	@ResponseBody
+	public String doctor4() {
+		return "Paul McGann";	
+	
+		}
+
+}
